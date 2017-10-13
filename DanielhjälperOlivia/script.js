6d21a3ae778fb9234f08155503867882bece5611
oliviaengstrom <31522628+oliviaengstrom@users.noreply.github.com>
2017-10-13 10:08:04 +0200

    Add files via upload

---
+++ DanielhjälperOlivia/script.js
@@ -0,0 +1,25 @@
+
+console.log("hello");
+alert("you wanna see me disappear and come back really fast?");
+console.log(10*10);
+console.log(2+5);
+console.log (7/8);
+console.log(Math.random());
+
+document.getElementById('pop').innerHTML = "";
+
+if (4>5){
+	console.log("it is true");
+}
+
+else {
+	console.log("it is false!");
+}
+
+function printApples(nrApples) {
+	while (x<nrApples){
+
+	}
+}
+
+printApples(10);
